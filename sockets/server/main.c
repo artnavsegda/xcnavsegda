c781c882cbc92c34294a5dcf89279d029bb7209e
Art Navsegda <artnavsegda@gmail.com>
2017-01-09 02:25:17 +0300

    Server

---
+++ sockets/server/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  server
+//
+//  Created by Art Navsegda on 1/9/17.
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
