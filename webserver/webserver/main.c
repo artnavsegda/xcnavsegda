89a631c56dbc77cbf8b617ffbd25f12c3aa6fbe1
Art Navsegda <artnavsegda@gmail.com>
2017-02-09 05:36:47 +0300

    webserver

---
+++ webserver/webserver/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  webserver
+//
+//  Created by Art Navsegda on 2/9/17.
+//  Copyright © 2017 Art Navsegda. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
