d2a09c0b304a0c56274b4b61bd3580c11e256910
Art Navsegda <artnavsegda@gmail.com>
2017-02-09 06:05:07 +0300

    server

--- webserver/webserver/main.c
+++ webserver/webserver/main.c
@@ -1,15 +1,29 @@
-//
-//  main.c
-//  webserver
-//
-//  Created by Art Navsegda on 2/9/17.
-//  Copyright © 2017 Art Navsegda. All rights reserved.
-//
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+void drop(int dropstatus, char *dropdesc)
+{
+    if (dropstatus == -1)
+    {
+        perror(dropdesc);
+        exit(1);
+    }
+}
+
+void drop2(char *dropstatus, char *dropdesc)
+{
+    if (dropstatus == NULL)
+    {
+        perror(dropdesc);
+        exit(1);
+    }
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    int sock = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+    drop(sock,"socket error");
     return 0;
 }
