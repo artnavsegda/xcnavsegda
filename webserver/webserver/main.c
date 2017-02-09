b987bdb0152d26a39a344cca219fae52885c99a9
Art Navsegda <artnavsegda@gmail.com>
2017-02-09 07:07:15 +0300

    webserver

--- webserver/webserver/main.c
+++ webserver/webserver/main.c
@@ -22,8 +22,21 @@ void drop2(char *dropstatus, char *dropdesc)
 }
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
     int sock = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
     drop(sock,"socket error");
+    int reuseaddr = 1;
+    setsockopt(sock,SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(reuseaddr));
+    struct sockaddr_in server = {
+        .sin_family = AF_INET,
+        .sin_addr.s_addr = INADDR_ANY,
+        .sin_port = htons(1100)
+    };
+    drop(bind(sock,(struct sockaddr *)&server,sizeof(server)),"bind error");
+    drop(listen(sock,10),"listen error");
+    while (1)
+    {
+        int msgsock = accept(sock,NULL,NULL);
+        
+    }
     return 0;
 }
